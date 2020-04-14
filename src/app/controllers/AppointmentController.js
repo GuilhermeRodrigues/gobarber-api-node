c3d05495cbd71e40741a3f8a27960a3213e41cfc
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2020-04-14 09:29:08 -0300

    Bloquear agendamentos para si mesmo
    Bloquear agendamentos para si mesmo.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -58,6 +58,13 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
+    // Check if user is scheduling with himself
+    if (req.userId === provider_id) {
+      return res
+        .status(401)
+        .json({ error: 'You cannot schedule with yourself' });
+    }
+
     // Check for past dates
     const hourStart = startOfHour(parseISO(date));
 
